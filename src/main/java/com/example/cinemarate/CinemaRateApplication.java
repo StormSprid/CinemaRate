ac87e7567508a504a2c684659d5ddf54d0cb0fae
Emil Galiyev <emil333.mem@gmail.com>
2025-07-02 15:03:18 +0500

    Build:add .env and .gitignore

---
+++ src/main/java/com/example/cinemarate/CinemaRateApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemarate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinemarate/CinemaRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemarate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
