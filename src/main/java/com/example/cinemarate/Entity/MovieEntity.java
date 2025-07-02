5b3985554beddb5dabf638ee78a1e6aee7bfd0e6
Emil Galiyev <emil333.mem@gmail.com>
2025-07-02 19:07:19 +0500

    add lombok @getter & @setter

--- src/main/java/com/example/cinemarate/Entity/MovieEntity.java
+++ src/main/java/com/example/cinemarate/Entity/MovieEntity.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+
 public class MovieEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +21,7 @@ public class MovieEntity {
     String description;
     int year;
     //TODO create a new table with review per film
-    //List<RewiewEntity> rewiews;
+    List<RewiewEntity> rewiews;
 
 
     @Override
---
+++ src/main/java/com/example/cinemarate/Entity/RewiewEntity.java
@@ -0,0 +1,7 @@
+package com.example.cinemarate.Entity;
+
+public class RewiewEntity {
+    Long id;
+    int rating;
+    int userId;
+}
