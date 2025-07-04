b744ada34c8622ab2967a3fb7deacf172789058c
Emil Galiyev <emil333.mem@gmail.com>
2025-07-04 18:11:13 +0500

    feat:add deleteMovie method to MovieServiceImpl

--- src/main/java/com/example/cinemarate/ServiceImpl/MovieServiceImpl.java
+++ src/main/java/com/example/cinemarate/ServiceImpl/MovieServiceImpl.java
@@ -1,28 +1,43 @@
 package com.example.cinemarate.ServiceImpl;
 
 import com.example.cinemarate.Entity.MovieEntity;
+import com.example.cinemarate.Repository.MovieRepository;
 import com.example.cinemarate.Service.MovieService;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+    @Autowired
+    public MovieServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
     @Override
-    public MovieEntity getMovieById(Long id) {
+    public MovieEntity createMovie(MovieEntity movie) {
         return null;
     }
 
     @Override
-    public MovieEntity save(MovieEntity m) {
+    public MovieEntity updateMovie(MovieEntity movie, Long id) {
         return null;
     }
 
     @Override
-    public List<MovieEntity> allMovies() {
-        return List.of();
+    public void deleteMovie(Long id) {
+        Optional<MovieEntity> m = movieRepository.findById(id);
+        movieRepository.delete(m.orElseThrow(() -> new EntityNotFoundException(
+                String.format("No movie with id %d was found",id)
+        ))
+        );
     }
 
-    @Override
-    public void deleteMovie(MovieEntity m) {
-
-    }
 }
+
