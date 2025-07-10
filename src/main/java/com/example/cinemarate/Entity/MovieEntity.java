67f08b9d289c2a1fd48a674fae1873c23789076e
Emil Galiyev <emil333.mem@gmail.com>
2025-07-10 16:56:06 +0500

    refactor:refactor MovieEntity create factory method

--- src/main/java/com/example/cinemarate/Entity/MovieEntity.java
+++ src/main/java/com/example/cinemarate/Entity/MovieEntity.java
@@ -13,13 +13,13 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "movie")
 public class MovieEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
      private Long  id;
     private String title;
     private String description;
@@ -27,7 +27,7 @@ public class MovieEntity {
     private String posterUrl;
     //TODO create a new table with review per film
     @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL)
-
+    @Setter
     private List<ReviewEntity> reviews = new ArrayList<>();
     @Transient
     private double meanRating = -1;
@@ -35,34 +35,46 @@ public class MovieEntity {
     //TODO refactor with setters not this.
     public static MovieEntity create(String title,String description,int year,String posterUrl){
         MovieEntity m = new MovieEntity();
-        if(title.isBlank()){
-            throw  new IllegalArgumentException(String.format("Title %s is not valid.Error to create a Movie.",title));
-        }
-        else {
-            m.title = title;
-        }
-        if(description.isBlank()){
-            throw  new IllegalArgumentException(String.format("Description %s is not valid.Error to create a Movie.",description));
-        }
-        else {
-            m.description = description;
-        }
-        if (year < 1900 || year > 2030){
-            throw new IllegalArgumentException(String.format("Year %s is not valid.Error to create a Movie.",year));
-        }
-        else{
-            m.year = year;
-        }
-        if(posterUrl.isBlank()){
-            throw  new IllegalArgumentException(String.format("PosterURL %s is not valid.Error to create a Movie.",posterUrl));
-        }
-        else{
-            m.posterUrl = posterUrl;
-        }
+        m.setTitle(title);
+        m.setDescription(description);
+        m.setYear(year);
+        m.setPosterUrl(posterUrl);
 
      return m;
     }
 
+
+
+    public void setTitle(String title) {
+        if(title.isBlank()){
+            throw  new IllegalArgumentException(String.format("Title %s is not valid.Error to create a Movie.",title));
+        }
+        this.title = title;
+
+    }
+
+    public void setDescription(String description) {
+        if(description.isBlank()){
+            throw  new IllegalArgumentException(String.format("Description %s is not valid.Error to create a Movie.",description));
+        }
+
+            this.description = description;
+
+    }
+
+    public void setYear(int year) {
+        if (year < 1900 || year > 2030){
+            throw new IllegalArgumentException(String.format("Year %s is not valid.Error to create a Movie.",year));
+        }
+        this.year = year;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        if(posterUrl.isBlank()){
+            throw  new IllegalArgumentException(String.format("PosterURL %s is not valid.Error to create a Movie.",posterUrl));
+        }
+        this.posterUrl = posterUrl;
+    }
 
     @Override
     public String toString() {
