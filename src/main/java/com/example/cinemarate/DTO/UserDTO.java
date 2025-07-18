f1b4773bcaf646ce4000edcc488a0cbcdbfa68e2
Emil Galiyev <emil333.mem@gmail.com>
2025-07-18 12:47:21 +0500

    feat:add NotNull to UserDto

--- src/main/java/com/example/cinemarate/DTO/UserDTO.java
+++ src/main/java/com/example/cinemarate/DTO/UserDTO.java
@@ -2,11 +2,17 @@ package com.example.cinemarate.DTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDTO {
+    @NotNull(message = "Username is mandatory")
+    @Size(max = 30)
     private String username;
+    @NotNull(message = "Email is mandatory")
+    @Size(max = 30)
     private String email;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
