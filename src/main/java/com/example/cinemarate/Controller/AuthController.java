b0238287af44f90eb1742737a4eb514387ef22fd
Emil Galiyev <emil333.mem@gmail.com>
2025-07-31 15:25:23 +0500

    feat:full jwt implementation

--- src/main/java/com/example/cinemarate/Controller/AuthController.java
+++ src/main/java/com/example/cinemarate/Controller/AuthController.java
@@ -1,4 +1,62 @@
 package com.example.cinemarate.Controller;
 
+import com.example.cinemarate.DTO.SignInRequest;
+import com.example.cinemarate.DTO.SignUpRequest;
+import com.example.cinemarate.Entity.UserEntity;
+import com.example.cinemarate.Repository.UserRepository;
+import com.example.cinemarate.Security.jwt.JwtCore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtCore jwtCore;
+
+
+    @PostMapping("/signup")
+    ResponseEntity<?> signup(@RequestBody SignUpRequest signUpRequest){
+        if(userRepository.existsByUsername(signUpRequest.getUsername())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Name already registered");
+        }
+        if(userRepository.existsByEmail(signUpRequest.getEmail())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already registered");
+        }
+        String hashed = passwordEncoder.encode(signUpRequest.getPassword());
+        UserEntity userEntity = UserEntity.create(
+                signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                hashed
+        );
+        userRepository.save(userEntity);
+        return ResponseEntity.ok("User registered");
+    }
+    @PostMapping("/signin")
+    ResponseEntity<?>   signIn(@RequestBody SignInRequest signInRequest){
+        Authentication authentication = null;
+        try {
+            authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signInRequest.getUsername(),signInRequest.getPassword()));
+
+        }catch (BadCredentialsException e){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtCore.generateToken(authentication);
+        return ResponseEntity.ok(jwt);
+    }
 }
--- src/main/java/com/example/cinemarate/Controller/UserController.java
+++ src/main/java/com/example/cinemarate/Controller/UserController.java
@@ -1,75 +1,43 @@
 package com.example.cinemarate.Controller;
-
 import com.example.cinemarate.Converter.UserConverter;
+import com.example.cinemarate.DTO.SignInRequest;
+import com.example.cinemarate.DTO.SignUpRequest;
 import com.example.cinemarate.DTO.UserDTO;
 import com.example.cinemarate.Entity.UserEntity;
 import com.example.cinemarate.Repository.UserRepository;
-//import com.example.cinemarate.ServiceImpl.SessionServiceImpl;
+import com.example.cinemarate.Security.jwt.JwtCore;
 import com.example.cinemarate.ServiceImpl.UserServiceImpl;
-import jakarta.servlet.http.Cookie;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
-import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
 
     private final UserServiceImpl service;
 
     private  final UserConverter converter;
 
-//    private final SessionServiceImpl sessionService;
 
-
-//    @GetMapping("/me/name")
-//    public String getMyName(@RequestParam UUID uuid){
-//        return sessionService.getNameById(uuid);
-//    }
 
     @GetMapping("/{id}")
     public Optional<UserDTO> getUser(@PathVariable Long id) {
-        return Optional.ofNullable(converter.toDto(repository.findById(id)));
-    }
-
-    @PostMapping("/registration")
-    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserDTO dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.register(dto));
-    }
-    @PostMapping("/login")
-    public ResponseEntity<UserDTO> login(@RequestBody UserDTO dto ){
-//        Cookie cookie = new Cookie("sessionId", dto.getSessionId().toString());
-//        cookie.setHttpOnly(true);  // защита от XSS
-//        cookie.setPath("/");       // доступна для всех URL
-//        cookie.setMaxAge(7 * 24 * 60 * 60); // неделя
-//
-//        response.addCookie(cookie);
-        return ResponseEntity.ok(service.login(dto));
+        return Optional.ofNullable(converter.toDto(userRepository.findById(id)));
     }
 
 
-//    @GetMapping("/getName")
-//    public ResponseEntity<String> getUserName(@RequestHeader("X-Session-Id") UUID id ){
-//        System.out.println("Get name trigger");
-//        return ResponseEntity.status(HttpStatus.OK).body(sessionService.getNameById(id));
-//    }
-//    @PostMapping("/logout")
-//    public ResponseEntity<String> logoutUser(@RequestParam UUID uuid){
-//        if(sessionService.logout(uuid)){
-//            return ResponseEntity.ok("Logout successful");
-//
-//        } else{
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Session not found");
-//        }
-//    }
+
 }
